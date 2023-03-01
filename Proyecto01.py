af58e5c22210103c9795dc34546336f063a35afc
Yastin Membrenio <membreno.yastin14@gmail.com>
2023-02-28 23:41:31 -0600

    En esta ocasion se integraron comentarios a cada funcion, ademas de una nueva funcion al archivo

--- Proyecto01.py
+++ Proyecto01.py
@@ -1,4 +1,10 @@
 #Modulo que contiene funciones de conversiones
 
+# Esta funcion retorna el valor de un angulo de grados a radianes
 def deg2rad(x):
 	return x*3.1416/180
+
+# Esta funcion retorna el valor de un kilogramo fuerza a newtons
+def kgf1Nwt(x):
+	return x*9.81
+
