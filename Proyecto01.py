1823cc836380b6967cfd16bf0debe81849577d75
Yastin Membrenio <membreno.yastin14@gmail.com>
2023-03-01 19:16:19 -0600

    Se añadieron dos funciones, en especial una funcion original :o

--- Proyecto01.py
+++ Proyecto01.py
@@ -1,17 +1,10 @@
 #Modulo que contiene funciones de conversiones
 
-# Esta funcion retorna el valor de un angulo de grados a radianes
 def deg2rad(x):
 	return x*3.1416/180
-
-# Esta funcion retorna el valor de un kilogramo fuerza a newtons
-def kgf1Nwt(x):
-	return x*9.81
-
-# Esta funcion retorna de pulgadas a centimetros 
-def in2cm(x):
-	return x*2.54
-
-# Esta funcion retorna de kilogramos a newton
-def kgf2cm(x):
-	return x*9.81
+	
+def marginal(x,y):
+	return [2*x,3*y]
+	
+def original_function():
+	print("Este es un mensaje original")
