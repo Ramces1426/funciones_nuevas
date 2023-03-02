fd57954c3ca31603b11b72656c588dfa67268c2d
Yastin Membrenio <membreno.yastin14@gmail.com>
2023-03-01 19:51:33 -0600

    Corrigiendo espacios en la rama master

--- Proyecto01.py
+++ Proyecto01.py
@@ -2,14 +2,12 @@
 
 def deg2rad(x):
 	return x*3.1416/180
-	
+
 def marginal(x,y):
 	return [2*x,3*y]
-	
+
 def original_function():
 	print("Este es un mensaje original")
 # Añadiendo documentacion a la funcion master	
 def master():
 	print("Esta funcion esta en la rama master")
-	
-
